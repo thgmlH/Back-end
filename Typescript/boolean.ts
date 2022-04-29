19c05b694bf4c425551d96d2c4a2457aaeb971e2
thgmlH <80466573+thgmlH@users.noreply.github.com>
2022-04-29 14:56:25 +0900

    Update boolean.ts

--- Typescript/boolean.ts
+++ Typescript/boolean.ts
@@ -12,5 +12,7 @@ let age: number = 37;
 let sentence: string = `Hello, my name is ${ fullName }.
 I'll be ${ age + 1 } years old next month.`;
 
+console.log(sentence)
+
 //let sentence: string = "Hello, my name is " + fullName + ".\n\n" +
 //    "I'll be " + (age + 1) + " years old next month.";
