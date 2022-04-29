905781ea403831ca1b6349eabd76ff61f3c183d8
thgmlH <80466573+thgmlH@users.noreply.github.com>
2022-04-29 14:45:31 +0900

    Create boolean.ts

---
+++ Typescript/boolean.ts
@@ -0,0 +1,16 @@
+//Boolean
+let isDone: boolean = false;
+//Nimber
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744
+//String
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+
+let sentence: string = `Hello, my name is ${ fullName }.
+I'll be ${ age + 1 } years old next month.`;
+
+//let sentence: string = "Hello, my name is " + fullName + ".\n\n" +
+//    "I'll be " + (age + 1) + " years old next month.";
