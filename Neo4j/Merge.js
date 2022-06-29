df3e74bb3dddd9ae741256498bc4f6cbf81016c4
thgmlH <80466573+thgmlH@users.noreply.github.com>
2022-06-29 17:07:41 +0900

    Update Merge.js

--- Neo4j/Merge.js
+++ Neo4j/Merge.js
@@ -7,22 +7,22 @@ exports.handler = (event, context) => {
      var result = null
      var count = null
 
-     result = session.run('MATCH (n:Person{serial_number: $serial_number}) RETURN COUNT(n)', {serial_number: event['serial_number']})
+     result = session.run('MATCH (n:Person{number: $number}) RETURN COUNT(n)', {number: event['number']})
      
      //result = result.records[0].get(0)
 
      const r = result.records(record=> record._fields[0].low)
-     //serial_number 1개 이상 있음 => 다른 속성 노드 有
-     //serial_number 0개 => CREATE
+     //number 1개 이상 있음 => 다른 속성 노드 有
+     //number 0개 => CREATE
      
      if(count >= 1){
-          result = session2.run('MATCH (a:Person {serial_number: $serial_number}) WHERE a.name <> $name MERGE (n:Person {serial_number: $serial_number, name: $name}) ON CREATE SET n.inactive=0, a.inactive = 1, n.sex = $sex RETURN n, a'
-          , {serial_number: event['serial_number'], name: event['name'], sex: event['sex']})
+          result = session2.run('MATCH (a:Person {number: $number}) WHERE a.name <> $name MERGE (n:Person {number: $number, name: $name}) ON CREATE SET n.inactive=0, a.inactive = 1, n.sex = $sex RETURN n, a'
+          , {number: event['number'], name: event['name'], sex: event['sex']})
           console.log('1 이상')
      }
      else{
-          result = session2.run('CREATE (a:Person {serial_number: $serial_number, name:$name, sex:$sex}) RETURN a',
-                                    {serial_number: event['serial_number'], name: event['name'], sex: event['sex']})
+          result = session2.run('CREATE (a:Person {number: $number, name:$name, sex:$sex}) RETURN a',
+                                    {number: event['number'], name: event['name'], sex: event['sex']})
           console.log('0')
      }
 
